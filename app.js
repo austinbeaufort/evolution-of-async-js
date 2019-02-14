e7a819776d73bfef5d9468cdc537f6efbcba9906
Austin Beaufort <tinyhatcoding@gmail.com>
2019-02-13 19:34:43 -0500

    Initial Commit: Learning Asynchronous JavaScript

---
+++ app.js
@@ -0,0 +1,20 @@
+// function saveToTheDb(value) {
+//     return new Promise(function(resolve, reject) {
+//         db.values.insert(value, function(err, user) {
+
+//             if(err) {
+//                 return reject(err);
+//             }
+//             resolve(user);
+//         })
+//     }
+// }
+
+// async function save(Something) {
+//     try {
+//         await Something.save()
+//     } catch (ex) {
+
+//     }
+//     console.log('success');
+// }
